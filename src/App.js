25367e7e330ba8879106311eff47a035163850dd
Caleb Smith <calebjsmith2101@gmail.com>
2022-06-29 18:51:19 +1000

    add tours

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
   const [loading,setLoading]= useState(true);
   const [tours,setTours]= useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id );
+    setTours(newTours);
+  }
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -35,7 +40,7 @@ function App() {
     )
   }
   return ( <main>
-     <Tours />
+     <Tours tours={tours} removeTour={removeTour} />
   </main>
   );
 }
